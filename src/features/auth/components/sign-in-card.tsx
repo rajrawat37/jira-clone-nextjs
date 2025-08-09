9d46e8f8d4d68da04124381324516135cbaef365
Raj Rawat <rawatraj995@gmail.com>
2025-08-09 15:47:25 +0530

    feat: zod validation, react-hook-form and comments

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,13 +1,49 @@
 import React from 'react'
 import {Card,CardContent,CardFooter,CardHeader,CardTitle} from "@/components/ui/card";
+import {Form,FormControl,FormField,FormItem,FormLabel,FormMessage} from "@/components/ui/form";
 import { DottedSeparator } from '@/components/dottted-separator';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import { useForm } from 'react-hook-form';
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
 import {FcGoogle} from "react-icons/fc";
 import {FaGithub} from "react-icons/fa";
 
 
+//TypeScript → prevents you from writing code for fields that don’t exist in your schema.
+// Zod → prevents users from entering invalid data into those fields.
+
+
+// This is the skeleton of your form — the rules for what fields exist and what they must look like.
+// This is runtime validation logic — Zod will actually check the values later.
+const formSchema = z.object({
+  email: z.email({message: "Invalid email address"}),
+  password: z.string().min(8, {message: "Password must be at least 8 characters"}),
+});
+
+
+//type formData is just a compile-time contract for your code.
+// When your app runs in the browser, TypeScript’s types don’t exist anymore — they’re stripped out during compilation to plain JavaScript.
+type formData = z.infer<typeof formSchema>;
+
 const SignInCard = () => {
+
+    // useForm() is a Hook hence it must be written inside functional component
+    // TypeScript enforces your form matches that structure at compile time.
+    const form = useForm<formData>({
+        resolver: zodResolver(formSchema), // Zod enforces the same rules at runtime.
+        defaultValues: {
+          email: "",
+          password: "",
+        },
+    });
+  
+
+  const onSubmit = (data: formData) => {
+    console.log(data);
+  };
+
   return (
     <Card className='w-full h-full md:w-[487px] border-none shadow-none text-center'>
         <CardHeader className='flex items-center justify-center text-center p-7'>
@@ -19,28 +55,38 @@ const SignInCard = () => {
             <DottedSeparator/>
         </div>
         <CardContent className="p-7 ">
-           <form action="" className='space-y-4 '>
-            <Input
-             required
-             type='email'
-             value={""}
-             placeholder='Enter email address'
-             onChange={() => {}}
-             disabled={false}
-            />
-            <Input
-             required
-             type='password'
-             value={""}
-             placeholder='Enter password'
-             onChange={() => {}}
-             disabled={false}
-            />
-            <Button disabled={false} type='submit' className='w-full'>
-              Sign In
-            </Button>
-           </form>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4 '>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({field}) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl></FormControl>
+                    <Input
+                    required
+                    type='email'
+                    placeholder='Enter email address'
+                    {...form.register("email")}
+                    disabled={false}
+                    />
+                    <Input
+                    required
+                    type='password'
+                    placeholder='Enter password'
+                    {...form.register("password")}
+                    disabled={false}
+                    />
+                    <Button disabled={false} type='submit' className='w-full'>
+                      Sign In
+                    </Button>
+                  </FormItem>)}
+                  ></FormField>
+            </form>
+          </Form>
         </CardContent>
+        
         <div className="px-6">
           <DottedSeparator/>
         </div>
@@ -59,3 +105,41 @@ const SignInCard = () => {
 }
 
 export default SignInCard
+
+
+
+
+
+// Without Zod, we cna still achieve compile-time safety in react-hook-form by manually defining a Typescript type for the form data insted of inferring it
+
+/*
+
+type formData = {
+  email : string;
+  password : string;
+}
+
+const form = useForm<FormData>({
+    defaultValues: {
+       email: "",
+       password:"",
+    },  
+});
+
+*/
+
+
+/*
+
+How React Hook forms help here ?
+Without form hooks we would need to manage state for each field separately by useState() like :
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+We need to handle form submission,errors and reset also
+
+But React hook forms Reset, watch, and set values are built-in.
+It kind of reduce the boilerplate code and manual state handling, acts as an abstraction.
+Its basically a form state automation layer.
+
+*/
+
